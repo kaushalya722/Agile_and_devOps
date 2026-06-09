c03a2234539db898a5f4c1f90abee40cb4600a8f
kaushalya kumari <kkaushalya722@gmail.com>
2026-06-09 14:06:57 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/agile_and_devops/Agile_and_devOps.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agile_and_devops;
+
+/**
+ *
+ * @author Amila
+ */
+public class Agile_and_devOps {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
