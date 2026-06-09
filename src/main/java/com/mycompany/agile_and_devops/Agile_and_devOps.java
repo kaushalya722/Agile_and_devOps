221b05ca52642d986d3b32a075193c5a28dea1c4
kaushalya kumari <kkaushalya722@gmail.com>
2026-06-09 14:47:58 +0530

    Add comment

--- src/main/java/com/mycompany/agile_and_devops/Agile_and_devOps.java
+++ src/main/java/com/mycompany/agile_and_devops/Agile_and_devOps.java
@@ -11,6 +11,8 @@ package com.mycompany.agile_and_devops;
 public class Agile_and_devOps {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); 
+        //comment
+        System.out.println("Hello"); 
     }
 }
